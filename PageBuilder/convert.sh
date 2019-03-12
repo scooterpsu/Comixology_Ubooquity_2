4bfa6e321ccdb2d83960d1e157e674a1c879f712
scooterpsu <3433982+scooterpsu@users.noreply.github.com>
2019-03-11 21:12:16 -0400

    Update convert.sh to convert folder-info.html into series.json
    Simple syntax:
    convert.sh /path/to/folder-info.html
    
    CAUTION:
    This deletes folder-info.html and folder.css when it's done, if you're automating backup your publisher files.

--- PageBuilder/convert.sh
+++ PageBuilder/convert.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-jsonConvert=`cat $1 | pup '.cell json{}'`
-hrefs=`echo $jsonConvert | jq '.[].children[].children[0].href | select(length > 0)'`
-onclicks=`echo $jsonConvert | jq '.[].children[].children[0].onclick | select(length > 0)'| sed s/"\&#39;"/"'"/g`
-pageIDs=`printf "$hrefs" | cut -d \/ -f 4`
-rawfilenames=`echo $jsonConvert | jq '.[].children[].children[0].children[0].src | select(length > 0)'`
-issueTest=`echo "$pageIDs" | sed -n "1p"`
-pageLabels=`echo $jsonConvert | jq .[].children[1].text`
-cellDataArray=`paste -d',' <(printf "${hrefs}") <(printf "${onclicks}") <(echo "${rawfilenames}") <(printf "${pageLabels}")`
-echo "$cellDataArray" > output.csv
+my_dir=`dirname $0`
+inputHTML=`cat "$1"`
+name=`echo $inputHTML | $my_dir/pup '#title text{}' | xargs`
+if ([[ $name == *"("* ]]) && ([[ "${name: -1}" == ")" ]]); then
+start_year=`echo $name | rev | cut -d \( -f1 | cut -c 2- | rev`
+name=`echo $name | sed -e "s/ ($start_year)$//"`
+fi
+description=`echo $inputHTML | $my_dir/pup '#desc text{}'`
+publisher=`echo $inputHTML | $my_dir/pup '#publisher text{}'`
+seriesType="comicSeries"
+JSON_STRING=$($my_dir/jq -n \
+    --arg desc "$description" \
+    --arg name "$name" \
+    --arg year "$start_year" \
+    --arg pub "$publisher" \
+    --arg type "$seriesType" \
+    '{metadata:[{description: $desc, name: $name, year: $year, publisher: $pub, type: $type}]}' )
+echo $JSON_STRING > $my_dir/temp/series.json
+mv $my_dir/temp/series.json "$(dirname "$1")/"
+rm "$(dirname "$1")/folder.css"
+rm "$(dirname "$1")/folder-info.html"
