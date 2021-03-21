fbc2285a147ee0835bfa07e0a4343226bb1f2ca8
scooterpsu <3433982+scooterpsu@users.noreply.github.com>
2021-03-21 16:20:20 -0400

    showRandom not properly showing random comics/books if enabled

--- comixology2/themeScript.js
+++ comixology2/themeScript.js
@@ -654,8 +654,9 @@ loadScript(proxyPrefix+"/theme/js/jquery-3.3.1.min.js", function(){
                 if(!settingsJSON['isOpdsProviderEnabled']){
                     $('#menuitem_mobile').remove();
                 }
-				if(!settingsJSON['showRandom']){
-					$('#submenuitem_browse_randomBooks, #submenuitem_browse_randomComics').remove();
+				if(!showRandom){
+					$('#submenuitem_browse_randomBooks').remove(); 
+					$('#submenuitem_browse_randomComics').remove();
 				}
 
                 fixPaths('.top-navigation a', 'href');
