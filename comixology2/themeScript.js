6f001b2db7ea050f60b48d2a29e2e463819a5691
Budlyte <yourpocketlint@gmail.com>
2021-03-20 18:51:17 -0700

    Update themeScript.js

--- comixology2/themeScript.js
+++ comixology2/themeScript.js
@@ -366,7 +366,12 @@ loadScript(proxyPrefix+"/theme/js/jquery-3.3.1.min.js", function(){
                                     getSeriesJson('?folderinfo=series.json');
                                     if($('.publisherContainer').length){
                                         $(document).ajaxStop(function(){
-                                            var publisher = $('#cmx_breadcrumb a:eq(1)').text();
+                                            var curloc = 2
+                                            if ($('#cmx_breadcrumb a').length > 0){
+                                                var curloc = $('#cmx_breadcrumb a').length - 1;
+                                            }
+                                            var publisher = $('#cmx_breadcrumb a:eq('+curloc+')').text();
+                                            publisher = publisher.replace('_', '');
                                             $('.publisher').text(publisher);
                                             $('#pubImg').attr('title',publisher);
                                         })
